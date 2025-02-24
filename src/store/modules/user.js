33ab305014cc1d25fcc31ed0d2b062ff079b3822
陈婷 <chentinghgn@163.com>
2025-02-24 15:43:46 +0800

    feat: token本地持久化；

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,39 +1,40 @@
 // 和用户相关的状态管理
 
-import {createSlice} from '@reduxjs/toolkit'
-import { request } from '@/utils'
+import { createSlice } from "@reduxjs/toolkit";
+import { request } from "@/utils";
 const useStore = createSlice({
-    name: 'user',
-    // 数据状态
-    initialState: {
-       token: '',
+  name: "user",
+  // 数据状态
+  initialState: {
+    token: localStorage.getItem("token_key") || "",
+  },
+
+  // 同步修改方法
+  reducers: {
+    setToken(state, action) {
+      state.token = action.payload;
+      // 本地存一份
+      localStorage.setItem("token_key", action.payload);
     },
-
-    // 同步修改方法
-    reducers: {
-        setToken(state,action) {
-            state.token = action.payload
-        }
-    }
-})
-
+  },
+});
 
 // 解构出actionCreater
-const { setToken } = useStore.actions
+const { setToken } = useStore.actions;
 
 // 获取reducers函数
-const useReducer = useStore.reducer
+const useReducer = useStore.reducer;
 
 // 异步方法  完成登录获取token
 
 const fetchLogin = (loginForm) => {
-    return async (dispatch) => {
-        // 1、发送异步请求
-        const res = await request.post('/authorizations', loginForm)
-        // 2、提交同步action进行token的存入
-        dispatch(setToken(res.data.token))
-    }
-}
+  return async (dispatch) => {
+    // 1、发送异步请求
+    const res = await request.post("/authorizations", loginForm);
+    // 2、提交同步action进行token的存入
+    dispatch(setToken(res.data.token));
+  };
+};
 
-export { fetchLogin, setToken}
-export default useReducer
+export { fetchLogin, setToken };
+export default useReducer;
