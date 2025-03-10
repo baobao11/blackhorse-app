5a78a681a6d3d322d1ed610ced041604919c82cb
陈婷 <chentinghgn@163.com>
2025-03-10 20:55:00 +0800

    feat: echarts基础图表渲染；

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,39 @@
+import * as echarts from "echarts";
+import { useEffect, useRef } from "react";
+
 const Home = () => {
-  return <div>Home</div>;
+  const chartRef = useRef(null);
+  useEffect(() => {
+    var chartDom = chartRef.current;
+    var myChart = echarts.init(chartDom);
+    var option;
+
+    option = {
+      xAxis: {
+        type: "category",
+        data: ["Vue", "React", "Angular"],
+      },
+      yAxis: {
+        type: "value",
+      },
+      series: [
+        {
+          data: [10, 40, 70],
+          type: "bar",
+        },
+      ],
+    };
+
+    option && myChart.setOption(option);
+  }, []);
+  return (
+    <div>
+      <div
+        id="main"
+        ref={chartRef}
+        style={{ width: "600px", height: "400px" }}
+      ></div>
+    </div>
+  );
 };
 export default Home;
